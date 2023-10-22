6b4ae95a4ad0355fdf36362f2f8af2ea461ca1f2
Robotix703 <stephane.chatelle@orange.fr>
2023-10-22 20:27:27 +0000

    Copy stargatecoalition

---
+++ README.md
@@ -0,0 +1,4 @@
+# abstractdonjon
+Abstract Donjon System for FoundryVTT
+
+See https://www.les12singes.com/75-abstract-donjon
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,29 @@
 const gulp = require('gulp');
 const zip = require('gulp-zip');
+const less = require('gulp-less');
+const del = require('del');
 
-exports.default = () => (
-	gulp.src(['*', '!build', '!node_modules', '!./gulpfile.js', '!LICENSE', '!package.json', '!package-lock.json', '!.gitignore'])
-		.pipe(zip('abstractdonjon.zip'))
+function clean() {
+	return del('build/**', {force:true});
+}
+
+function copyForZip() {
+	return gulp.src(['**/*', '!build/*', '!build', '!node_modules/**/*', '!node_modules', '!./gulpfile.js', '!LICENSE', '!package.json', '!package-lock.json', '!.gitignore'])
 		.pipe(gulp.dest('build'))
+}
+
+function compileLESS() {
+  	return gulp.src("styles/stargatecoalition.less")
+		.pipe(less())
+		.pipe(gulp.dest("./styles/"))
+}
+
+/* ----------------------------------------- */
+/*  Export Tasks
+/* ----------------------------------------- */
+
+exports.default = gulp.series(
+	clean,
+	compileLESS,
+	copyForZip
 );
